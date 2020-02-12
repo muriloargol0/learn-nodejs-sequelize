ab98e46650f30bf3682ed175f5667e9b92522980
Murilo Argolo <murilo.argolo@live.com>
2020-02-12 13:40:04 -0300

    new update

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,6 +1,12 @@
 const User = require('../models/User');
 
 module.exports = {
+    async index(req, res){
+        const users = await User.findAll();
+
+        return res.json(users);
+    },
+
     // That's an async function with the parameters req to REQUEST and res to RESPONSE
     async store(req, res){
         const { name, email } = req.body; // req.body is responsible for catch the values from a url request (promise)
--- src/database/index.js
+++ src/database/index.js
@@ -2,9 +2,14 @@ const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
 
 const User = require('../models/User');
+const Address = require('../models/Address');
 
 const cnn = new Sequelize(dbConfig);
 
 User.init(cnn);
+Address.init(cnn);
+Address.associate(cnn.models);
+
+// To automatize this step, we can use two tools: npm Consign or npm require-directory
 
 module.exports = cnn;
